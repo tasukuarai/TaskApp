1869c8dc66f208287a6e8c93eb7e32ce69189819
tasukuarai <j6312002@ed.tus.ac.jp>
2016-08-14 17:01:51 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/tasuku/arai/taskapp/TaskApp.java
@@ -0,0 +1,15 @@
+package jp.techacademy.tasuku.arai.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
